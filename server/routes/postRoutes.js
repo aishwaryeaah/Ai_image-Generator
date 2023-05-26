332b8384d7bfef048bef93af2c4426da3c28d3e0
Aishwarya <aaishwaryaaa1104@gmail.com>
2023-05-26 10:48:05 +0530

    Initial commit

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,43 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
+
+import Post from "../mongodb/models/post.js";
+
+dotenv.config();
+
+const router = express.Router();
+
+cloudinary.config({
+  cloud_name: "doigehlqm",
+  api_key: "638681827658423",
+  api_secret: "8igMPfL-Qe9AIfB44Fybrv6moF8",
+});
+
+router.route("/").get(async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({ success: true, data: posts });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Fetching posts failed, please try again" });
+  }
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.url,
+    });
+
+    res.status(200).json({ success: true, data: newPost });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Unable to create a post, please try again" });
+  }
+});
+
+export default router;
